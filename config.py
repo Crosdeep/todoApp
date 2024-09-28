846698d962ada54a3196d1e4a34660703bfe2f98
Crosdeep <pinarpinar959@mail.com>
2024-09-28 17:20:56 +0300

    Değişiklikler yapıldı

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard_to_guess_password'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://kisi:pp123@localhost/todoApp_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://kisi:pp123@localhost/todo_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
